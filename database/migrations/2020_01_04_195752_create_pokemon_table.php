8ec86d496af3a3d043b3392c0408d01e36f7b0cc
Tyler-Shockman <tyler.shockman@gmail.com>
2020-01-04 14:15:23 -0600

    Update 2020_01_04_195752_create_pokemon_table.php
    Modified the pokemon migration to include the desired columns, traits, and default values.

--- database/migrations/2020_01_04_195752_create_pokemon_table.php
+++ database/migrations/2020_01_04_195752_create_pokemon_table.php
@@ -15,7 +15,17 @@ class CreatePokemonTable extends Migration
     {
         Schema::create('pokemon', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->timestamps();
+            $table->string('name')->unique();
+            $table->json('types');
+            $table->integer('weight');
+            $table->integer('height');
+            $table->json('abilities');
+            $table->json('egg_groups');
+            $table->json('stats');
+            $table->string('genus');
+            $table->text('description');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));        
         });
     }
 
