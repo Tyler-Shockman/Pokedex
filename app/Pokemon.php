89a56bb99560ac0013bff6cc52903e4d32f16d0d
Tyler-Shockman <tyler.shockman@gmail.com>
2020-01-04 14:50:29 -0600

    Update Pokemon.php
    Implemented the neccessary portions of the Pokemon model to enable it to work as desired. Added casts property to cause json data for those fields to be automatically converted to array when working with pokemon and the fillable fields to enable seeding.

--- app/Pokemon.php
+++ app/Pokemon.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pokemon extends Model
 {
-    //
+    /**
+     * Causes the defined database fields (types, abilities, egg_groups, stats)
+     * to automatically cast from JSON to a php array when the are retrieved.
+     */
+    protected $casts = [
+        'types' => 'array',
+        'abilities' => 'array',
+        'egg_groups' => 'array',
+        'stats' => 'array',
+    ];
+
+    // Fillable fields.
+    protected $fillable = [
+        'id', 'name', 'types', 'height', 
+        'wieght', 'abilities', 'egg_groups', 
+        'states', 'genus', 'description'
+    ];
 }
